a2ef77d5359f49e082303478ccef171a6265e3a8
Taniita <tanya2001.sv@gmail.com>
2024-01-15 20:36:03 +0100

    JOINS.sql

---
+++ JOINS.sql
@@ -0,0 +1,52 @@
+create table City(
+    id serial primary key,
+    city_name varchar(40) unique not null
+);
+
+select * from City;
+
+
+
+create table Person(
+    id serial primary key,
+    person_name varchar(40),
+    city_id int not null
+);
+select * from Person;
+
+
+
+insert into City(city_name)
+values ('Kiyv'),
+       ('Lviv'),
+       ('Antalia'),
+       ('Moscow'),
+       ('Ivanovo'),
+       ('Berlin');
+      
+      
+insert into Person(person_name, city_id)
+values ('Vadim', 2),
+       ('Tanya', 4),
+       ('Alex', 6),
+       ('Anna', 3);
+      
+      
+insert into Person(person_name, city_id)
+values ('Inna', 8),
+       ('Anatoly', 10);
+
+      
+      
+ select Person.person_name, Person.city_id, City.city_name, City.id
+ from Person join City
+ on Person.city_id = City.id;
+
+
+ select Person.person_name, Person.city_id, City.city_name, City.id
+ from Person left join City
+ on Person.city_id = City.id;
+ 
+select Person.person_name, Person.city_id, City.city_name, City.id
+ from Person right join City
+ on Person.city_id = City.id;
